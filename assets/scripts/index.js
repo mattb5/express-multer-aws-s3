d054e701e7e93081aadfd137660ad8c3d7e6dcc2
mattb5 <mattbuffardi@gmail.com>
2016-07-08 15:58:32 -0400

    use javascript for uploading files
    use $.ajax instead of the browser's own POST for forms.  This prevents the
    page from reloading.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -5,3 +5,34 @@
 
 // use require without a reference to ensure a file is bundled
 require('./example');
+
+//document ready
+$(() => {
+  $('#application-x-www-form-urlencoded').on('submit', (event) => {
+    let getFormfields = require('../../lib/get-form-fields');
+
+  event.preventDefault();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://httpbin.org/post',
+    data: getFormfields(event.target),
+  })
+  .done((data) => console.log(data))
+  .fail((error) => console.error(error));
+});
+$('#multipart-form-data').on('submit', (event) => {
+  event.preventDefault();
+
+  $.ajax({
+    method: 'POST',
+    url: 'http://httpbin.org/post',
+    data: new FormData(event.target),
+    contentType: false,
+    processData: false,
+  })
+  .done((data) => console.log(data))
+  .fail((error) => console.error(error));
+});
+
+});
